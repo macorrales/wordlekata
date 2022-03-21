07a0d9484b5816f1d2abb44d34477d84d0cc5cee
Miguel Corrales <macorrales@users.noreply.github.com>
2022-03-21 15:18:19 +0000

    Return direct matches in any position

--- src/main/java/dojo/Wordle.java
+++ src/main/java/dojo/Wordle.java
@@ -11,7 +11,16 @@ public class Wordle {
     public Wordle(String word) {
         this.word = word.toLowerCase();
     }
+    
    public String guess(String guess){
-        return "feedback";
+
+        var feedback = new StringBuilder(".....");
+
+        for (int i=0;i<word.length();i++){
+            if (guess.charAt(i)==word.charAt(i)){
+                feedback.setCharAt(i,Character.toUpperCase(word.charAt(i)));
+            }
+        }
+        return  feedback.toString();
    }
 }
--- src/test/java/dojo/WordleTest.java
+++ src/test/java/dojo/WordleTest.java
@@ -18,4 +18,23 @@ public class WordleTest
         assertTrue(true);
         
     }
+
+    @Test 
+    public void when_noLetterMatches_expect_responseIs5dots(){
+        assertEquals(".....",new Wordle("HELLO").guess("xxxxx"));
+    }
+
+    @Test
+    public void when_firstLetterIsADirectMatch_expect_responseIsLetterInUppercase(){
+        assertEquals("A....",new Wordle("aaaaa").guess("abbbb"));
+
+    }
+
+    @Test
+    public void when_secondIsAdirectMatch_expect_responseIsThatLetterInUppercase(){
+        assertEquals(".A...",new Wordle("aaaaa").guess("babbb"));
+    }
+    
+
+    
 }
